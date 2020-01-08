36ebf1bc6de00cd555f9caca91fa120d4529c474
Chris8018 <tranuytrieuvi@protonmail.com>
2020-01-08 09:14:48 +0700

    Include tinyxml2 + success build

---
+++ ParsingXml/ParsingXml.cpp
@@ -0,0 +1,12 @@
+﻿// ParsingXml.cpp : Defines the entry point for the application.
+//
+
+#include "ParsingXml.h"
+//#include <tinyxml2.h>
+using namespace std;
+
+int main()
+{
+    cout << "Hello CMake." << endl;
+    return 0;
+}
--- ParsingXml/ParsingXml.h
+++ ParsingXml/ParsingXml.h
@@ -1,8 +1,10 @@
 ﻿// TestTinyXML2.h : Include file for standard system include files,
 // or project specific include files.
 
-#pragma once
+#ifndef ParsingXml_H
+#define ParsingXml_H
 
 #include <iostream>
 
 // TODO: Reference additional headers your program requires here.
+#endif
