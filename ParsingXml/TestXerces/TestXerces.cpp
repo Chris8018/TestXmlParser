cfaebbad7a55f811d3a48fac728fc5dff84806be
Chris8018 <tranuytrieuvi@protonmail.com>
2020-01-13 11:22:18 +0700

    Link Xerces lib

---
+++ ParsingXml/TestXerces/TestXerces.cpp
@@ -0,0 +1,24 @@
+#include "TestXerces.h"
+
+#include <iostream>
+#include <string>
+
+#include <xercesc/dom/DOM.hpp>
+
+//#include <xercesc/util/PlatformUtils.hpp>
+//#include <xercesc/util/XMLString.hpp>
+//#include <xercesc/dom/DOM.hpp>
+//#include <xercesc/util/OutOfMemoryException.hpp>
+//#include <xercesc/framework/XMLFormatter.hpp>
+//#include <xercesc/framework/LocalFileFormatTarget.hpp>
+//#include <xercesc/dom/DOMDocument.hpp>
+//#include <xercesc/dom/DOMImplementation.hpp>
+//#include <xercesc/dom/DOMImplementationRegistry.hpp>
+//#include <xercesc/dom/DOMLSSerializer.hpp>
+//#include <xercesc/dom/DOMLSOutput.hpp>
+
+int main()
+{
+    std::cout << "Hello CMake." << std::endl;
+    return 0;
+}
---
+++ ParsingXml/TestXerces/TestXerces.h
@@ -0,0 +1,6 @@
+#ifndef TextXMLPaser_TestXerces
+#define TextXMLPaser_TestXerces
+
+//
+
+#endif // !TextXMLPaser_TestXerces
