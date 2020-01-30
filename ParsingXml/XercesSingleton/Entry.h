c50d9ae80cbface1bc24e287549f2c6577781e12
Chris8018 <tranuytrieuvi@protonmail.com>
2020-01-30 16:38:33 +0700

    Create xml singleton test

---
+++ ParsingXml/XercesSingleton/Entry.h
@@ -0,0 +1,24 @@
+#ifndef XercesSingleton_H
+#define XercesSingleton_H
+
+class XmlDocument
+{
+    //
+};
+
+class XmlElement
+{
+    //
+};
+
+class XercesConfig
+{
+    //
+};
+
+class XmlLib
+{
+    //
+};
+
+#endif // !XercesSingleton_H
