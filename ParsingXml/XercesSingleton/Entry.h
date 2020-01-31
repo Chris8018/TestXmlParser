c1d9675c27553a2d40b02b3797d97d56ec398565
Chris8018 <tranuytrieuvi@protonmail.com>
2020-01-31 17:55:58 +0700

    Back up

--- ParsingXml/XercesSingleton/Entry.h
+++ ParsingXml/XercesSingleton/Entry.h
@@ -1,24 +1,104 @@
 #ifndef XercesSingleton_H
 #define XercesSingleton_H
 
+#include <memory>
+
+#include <string>
+
+#include <vector>
+#include <map>
+
+namespace xercesc_3_2
+{
+    class DOMImplementation;
+    class DOMDocument;
+    class DOMElement;
+    class DOMNode;
+    // TODO: Add more?
+}
+
 class XmlDocument
 {
+private:
+    std::shared_ptr<xercesc_3_2::DOMDocument> _xmlDocument;
+    std::string _encoding;
+public:
+    XmlDocument();
+    virtual ~XmlDocument();
+
     //
 };
 
 class XmlElement
 {
-    //
+private:
+    std::shared_ptr<xercesc_3_2::DOMNode> _parentNode;
+    std::shared_ptr<xercesc_3_2::DOMElement> _xmlElement;
+
+    bool _assigned = false;
+public:
+    XmlElement(std::shared_ptr<xercesc_3_2::DOMDocument> emptyDOMDocument, std::string name);
+    virtual ~XmlElement();
+
+    bool IsAssigned() const;
+
+    void SetText(const std::string &text);
+    void SetAttribute(const std::string &key, const std::string &value);
+    void SetAttributes(const std::map<std::string, std::string> &attributes);
+
+    void SetParentNode(std::shared_ptr<xercesc_3_2::DOMNode> parentNode);
+
+    std::string ToString() const;
+
+    //bool operator==(...) const;
+    //bool IsEqual(...) const;
 };
 
-class XercesConfig
+class XercesAdapter
 {
+    //friend XmlDocument;
+    //friend XmlElement;
+private:
+    XercesAdapter();
+    //static void Initialize();
+    //static void InitializeAdapter();
+    void InitializeDomImplementation();
+
+    void Clear();
+
+    //static std::shared_ptr<XercesAdapter> _adapter;
+    //static XercesAdapter * _adapter;
+
+    /*static shared_ptr<Demo> getInstance() {
+        static shared_ptr<Demo> d(new Demo);
+        return d;
+    }*/
+
+    /*static Demo &getInstance() {
+        static Demo d;
+        return d;
+    }*/
+
+    std::shared_ptr<xercesc_3_2::DOMImplementation> _domImpl;
+public:
+    XercesAdapter(XercesAdapter const &) = delete;
+    XercesAdapter& operator=(XercesAdapter const &) = delete;
+
+    virtual ~XercesAdapter();
+
+    static XercesAdapter& GetInstance();
+
+    std::shared_ptr<xercesc_3_2::DOMDocument> CreateEmptyDocument() const;
+
     //
 };
 
 class XmlLib
 {
-    //
+private:
+public:
+    static std::shared_ptr<XmlDocument> CreateXmlDocument(XmlElement &rootNode, const std::string &version, const std::string &encoding, const bool &standalone);
+    static std::shared_ptr<XmlElement> CreateXmlElement(const std::string &name, const std::string &text, const std::map<std::string, std::string> &attributes);
 };
 
 #endif // !XercesSingleton_H
--- ParsingXml/XercesSingleton/entry.cpp
+++ ParsingXml/XercesSingleton/entry.cpp
@@ -1,5 +1,10 @@
 #include "Entry.h"
 
+#include "xercesc/util/PlatformUtils.hpp"
+#include <xercesc/util/XMLString.hpp>
+
+#include "xercesc/dom/DOM.hpp"
+
 #include <iostream>
 
 int main()
@@ -7,3 +12,130 @@ int main()
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
+
+// XmlDocument------------------------------------------------------
+
+XmlDocument::XmlDocument()
+{
+    //
+}
+
+XmlDocument::~XmlDocument()
+{
+    //
+}
+
+//------------------------------------------------------------------
+
+// XmlElement-------------------------------------------------------
+
+XmlElement::XmlElement(std::shared_ptr<xercesc_3_2::DOMDocument> emptyDOMDocument, std::string name)
+{
+    this->_parentNode = emptyDOMDocument;
+    _xmlElement = 
+        std::shared_ptr<xercesc::DOMElement>(
+            emptyDOMDocument->createElement(
+                xercesc::XMLString::transcode(name.c_str())
+            )
+            );
+}
+
+XmlElement::~XmlElement()
+{
+    //
+}
+
+bool XmlElement::IsAssigned() const
+{
+    return _assigned;
+}
+
+void XmlElement::SetText(const std::string &text)
+{
+    //std::shared_ptr<
+}
+
+void XmlElement::SetAttribute(const std::string &key, const std::string &value)
+{
+    //
+}
+
+void XmlElement::SetAttributes(const std::map<std::string, std::string> &attributes)
+{
+    //
+}
+
+void XmlElement::SetParentNode(std::shared_ptr<xercesc::DOMNode> parent)
+{
+    //
+}
+
+std::string XmlElement::ToString() const
+{
+    return "";
+}
+
+//------------------------------------------------------------------
+
+// XmlLib-----------------------------------------------------------
+
+std::shared_ptr<XmlDocument> XmlLib::CreateXmlDocument(XmlElement &rootNode, const std::string &version, const std::string &encoding, const bool &standalone)
+{
+    return nullptr;
+}
+
+std::shared_ptr<XmlElement> XmlLib::CreateXmlElement(const std::string &name, const std::string &text, const std::map<std::string, std::string> &attributes)
+{
+    return nullptr;
+}
+
+//------------------------------------------------------------------
+
+// XercesAdapter----------------------------------------------------
+
+XercesAdapter::XercesAdapter()
+{
+    try
+    {
+        xercesc::XMLPlatformUtils::Initialize();
+    }
+    catch (std::exception & e)
+    {
+        // TODO: Print error message
+    }
+
+    // TODO: Create Parser, Implementation
+    InitializeDomImplementation();
+}
+
+XercesAdapter::~XercesAdapter()
+{
+    Clear();
+
+    // TODO: considering catching error
+    xercesc::XMLPlatformUtils::Terminate();
+}
+
+void XercesAdapter::Clear()
+{
+    _domImpl.reset();
+}
+
+void XercesAdapter::InitializeDomImplementation()
+{
+    if (_domImpl == nullptr)
+        _domImpl = std::shared_ptr<xercesc::DOMImplementation>(xercesc::DOMImplementationRegistry::getDOMImplementation(u""));
+}
+
+XercesAdapter& XercesAdapter::GetInstance()
+{
+    static XercesAdapter _xercesAdapter;
+    return _xercesAdapter;
+}
+
+std::shared_ptr<xercesc::DOMDocument> XercesAdapter::CreateEmptyDocument() const
+{
+    return std::shared_ptr<xercesc::DOMDocument>(this->_domImpl->createDocument(0, 0, 0));
+}
+
+//------------------------------------------------------------------
