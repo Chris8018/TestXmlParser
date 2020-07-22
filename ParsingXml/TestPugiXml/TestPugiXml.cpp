b830b20e40f6786b18b5ef568be1a8b62ca4a6bb
Chris8018 <tranuytrieuvi@protonmail.com>
2020-07-22 11:55:28 +0700

    minor test for pugi

--- ParsingXml/TestPugiXml/TestPugiXml.cpp
+++ ParsingXml/TestPugiXml/TestPugiXml.cpp
@@ -1,2 +1,23 @@
 #include "TestPugiXml.h"
 
+#include "pugixml.hpp"
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Test Pugi Xml" << std::endl;
+	pugi::xml_document doc;
+
+
+	//pugi::xml_node node1 = doc.append_child("root");
+	pugi::xml_node node1;
+	pugi::xml_node node2 = node1.append_child("child");
+
+	doc.append_child("root2");
+	//doc.append_child()
+	node1.set_name("aaaa");
+	std::cout << node1.name() << std::endl;
+
+	return 0;
+}
